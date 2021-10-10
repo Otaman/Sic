006d72637898f2d20203fcaadd32c0def97868c5
Serhii Buta <serhii.buta@gmail.com>
2021-10-10 22:52:39 +0200

    Implement background update strategy

--- src/Sic/Sic/Cache.cs
+++ src/Sic/Sic/Cache.cs
@@ -6,9 +6,19 @@ namespace Sic
     //ex.: new Cache().ClearAfter10Reads()
     public class Cache
     {
-        public static ICachingStrategy KeepForever(TimeSpan delayOnFailedLoads)
+        public static ICachingStrategy KeepForever(TimeSpan delayOnFailedLoads) => 
+            new KeepForever(delayOnFailedLoads);
+        
+        public static ICachingStrategy UpdateInBackground(TimeSpan delayWhenInUse, 
+            TimeSpan delayWhenNoUse, TimeSpan delayOnFailedLoads,
+            Action onUpdate = null, Action<Exception> onError = null)
         {
-            return new KeepForever(delayOnFailedLoads);
-        } 
+            var strategy = new UpdateInBackground(delayWhenInUse, delayWhenNoUse, delayOnFailedLoads);
+            
+            if (onUpdate != null) strategy.OnUpdate += onUpdate;
+            if (onError != null) strategy.OnError += onError;
+
+            return strategy;
+        }
     }
 }
--- src/Sic/Sic/CachedAsync.cs
+++ src/Sic/Sic/CachedAsync.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 
 namespace Sic
@@ -5,11 +6,16 @@ namespace Sic
     internal class CachedAsync<T>: ICachedAsync<T>
     {
         private Task<T> _getter;
+        public event Action OnGet;
         
         internal CachedAsync(Task<T> getter) => _getter = getter;
 
-        public Task<T> GetValue() => _getter;
+        public Task<T> GetValue()
+        {
+            OnGet?.Invoke();
+            return _getter;
+        }
 
-        internal void UpdateValue(Task<T> getter) => _getter = getter;
+        public void UpdateValue(Task<T> getter) => _getter = getter;
     }
 }
